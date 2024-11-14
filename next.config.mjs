c52c00195ea4c32355c98223537679da87eb3259
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-11-15 00:21:26 +0530

    fix middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      runtime: 'nodejs', // Set runtime to Node.js for compatibility with Clerk
+    },
+  };
+  
+  export default nextConfig;
+  
