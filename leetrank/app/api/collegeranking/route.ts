fdaa84eb6fa6964a241a200fc24bdf288320dd0d
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-11-14 18:47:18 +0530

    added force dynamic to the backend

--- leetrank/app/api/collegeranking/route.ts
+++ leetrank/app/api/collegeranking/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest,NextResponse } from "next/server";
 import prisma from "@/lib/db";
+export const dynamic = "force-dynamic";
+
 export async function POST(request:NextRequest) {
     try {
         const {searchParams}=new URL(request.url);
