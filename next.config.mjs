24c81032c3e63facd65b401174869f04c3f7df69
Himanshu-gupta31 <himanshu.gupta0031@gmail.com>
2024-11-15 16:19:23 +0530

    added gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      runtime: 'nodejs', // Set runtime to Node.js for compatibility with Clerk
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
   
